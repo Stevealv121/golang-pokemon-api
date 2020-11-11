bb6f52bb32ad6f87fce06679491e90a9a4f3d085
Steve <stevealv@hotmail.com>
2020-11-10 19:13:38 -0600

    Add new pokemons

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"io/ioutil"
 	"pokemon-api/database"
 )
 
@@ -17,6 +18,7 @@ func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.Use(commonMiddleware)
 	myRouter.HandleFunc("/pokemons", getAllPokemons).Methods("GET")
+	myRouter.HandleFunc("/pokemons", addNewPokemons).Methods("POST")
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
@@ -25,6 +27,14 @@ func commonMiddleware(next http.Handler) http.Handler {
 		w.Header().Add("Content-Type", "application/json")
 		next.ServeHTTP(w, r)
 	})
+}
+
+func addNewPokemons(w http.ResponseWriter, r *http.Request){
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var pokemon database.Pokemon
+	json.Unmarshal(reqBody, &pokemon)
+	database.PokemonDb = append(database.PokemonDb, pokemon)
+	json.NewEncoder(w).Encode(pokemon)
 }
 
 func main() {
